2dccc2677f150da72f33f7411358fd94d4a8788e
Douglas Motta <douglas1102rm@hotmail.com>
2025-03-11 18:08:39 +0000

    código base

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,12 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.lifecycle.runtime.compose.android)
+
     testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.jetbrains.kotlinx.coroutines.test)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
---
+++ app/src/test/java/com/example/testablearchmvvmandroid/MainDispatcherRule.kt
@@ -0,0 +1,25 @@
+package com.example.testablearchmvvmandroid
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainDispatcherRule(
+    val testScheduler: TestCoroutineScheduler = TestCoroutineScheduler()
+) : TestWatcher() {
+    private val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}
