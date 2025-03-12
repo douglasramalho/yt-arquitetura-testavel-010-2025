063ecfb00e267cc7459e3a8c75d495c8e28d125f
Douglas Motta <douglas1102rm@hotmail.com>
2025-03-12 19:07:01 +0000

    código final

---
+++ app/src/main/java/com/example/testablearchmvvmandroid/UserRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.example.testablearchmvvmandroid
+
+import android.content.Context
+import android.content.SharedPreferences
+import kotlinx.coroutines.delay
+
+class UserRepositoryImpl(context: Context) : UserRepository {
+
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+
+    override suspend fun getUserName(): String {
+        delay(2000)
+        return sharedPreferences.getString(PREF_NAME_KEY, PREF_NAME_DEFAULT) ?: PREF_NAME_DEFAULT
+    }
+
+    override fun saveUserName(name: String) {
+        sharedPreferences.edit().putString(PREF_NAME_KEY, name).apply()
+    }
+
+    companion object {
+        private const val PREF_NAME_KEY = "user_name"
+        private const val PREF_NAME_DEFAULT = "Usuário Padrão"
+    }
+}
